d7e996168fdfb5ca363c99fd2a196aa963f09d26
ErMenetto12 <matti@DESKTOP-SD1N06H>
2022-08-05 16:34:38 +0200

    0.1e05

--- src/main/java/net/mcreator/beforeandafterragnarok/init/BeforeAndAfterRagnarokModItems.java
+++ src/main/java/net/mcreator/beforeandafterragnarok/init/BeforeAndAfterRagnarokModItems.java
@@ -13,11 +13,15 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.beforeandafterragnarok.item.SunstonescrapItem;
+import net.mcreator.beforeandafterragnarok.item.SunstonepieceItem;
 import net.mcreator.beforeandafterragnarok.BeforeAndAfterRagnarokMod;
 
 public class BeforeAndAfterRagnarokModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, BeforeAndAfterRagnarokMod.MODID);
 	public static final RegistryObject<Item> SUNSTONEORE = block(BeforeAndAfterRagnarokModBlocks.SUNSTONEORE, CreativeModeTab.TAB_BUILDING_BLOCKS);
+	public static final RegistryObject<Item> SUNSTONEPIECE = REGISTRY.register("sunstonepiece", () -> new SunstonepieceItem());
+	public static final RegistryObject<Item> SUNSTONESCRAP = REGISTRY.register("sunstonescrap", () -> new SunstonescrapItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
